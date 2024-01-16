fd102d08eeeb502d070657777a81250c0119d5ca
jasen.zhangj <syzhangjunsy@163.com>
2024-01-16 15:29:23 +0800

    Update main.tf

--- examples/large-terraform/prod/main.tf
+++ examples/large-terraform/prod/main.tf
@@ -11,6 +11,10 @@ terraform {
   }
 }
 
+locals {
+  create_vpc = var.vpc_id == ""
+}
+
 module "network" {
   source = "../modules/network"
 
@@ -20,6 +24,24 @@ module "network" {
   azs  = var.azs
   public_subnets = var.public_subnets
 }
+
+data "aws_vpc" "selected" {
+  count = local.create_vpc ? 0 : 1
+
+  id = var.vpc_id
+}
+
+resource "aws_vpc" "this" {
+  count = local.create_vpc ? 1 : 0
+
+  cidr_block = var.cidr
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = try(data.aws_vpc.selected[0].id, aws_vpc.this[0].id)
+}
+
+
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
